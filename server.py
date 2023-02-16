f5fadad275a220c8130b4bf245cbf0904e64300d
Jack Lee <welsar55@gmail.com>
2023-02-16 13:33:24 -0600

    Add landing page

--- server.py
+++ server.py
@@ -11,6 +11,10 @@ def hello_world():
 def add_location():
     return render_template('add_location.html')
 
+@app.route("/landing") #TODO Change this to index.
+def landing():
+    return render_template('landing.html')
+
 # Helper for using vscode debugger
 if __name__ == "__main__":
     app.run(debug=True)
