428b9dc86e1feb53dea6bec5d9fcc6a761b30319
minizou <gh.mini.zou@gmail.com>
2023-02-15 23:39:56 -0600

    Add [Add Location] HTML outline

--- server.py
+++ server.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello World</p>"
 
+@app.route("/add")
+def add_location():
+    return render_template('add_location.html')
+
 # Helper for using vscode debugger
 if __name__ == "__main__":
     app.run(debug=True)
