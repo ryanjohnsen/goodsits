9b5901e91648b0f1ac30cd66d626bbf8b56430b1
minizou <gh.mini.zou@gmail.com>
2023-02-22 00:35:50 -0600

    Fix exitting modal, textarea expansion

--- static/scripts/reviewModal.js
+++ static/scripts/reviewModal.js
@@ -1,6 +1,11 @@
-function addReview(){
+function addReview() {
     const reviewModal=document.getElementById("hideReviewForm");
     reviewModal.style.display="block";
+}
+
+function closeModal() {
+    const reviewModal=document.getElementById("hideReviewForm");
+    reviewModal.style.display="none";
 }
 
 document.getElementById("addBtn").addEventListener("click", addReview);
