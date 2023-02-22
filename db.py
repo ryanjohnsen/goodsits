432b6cd69bcbabb1ad112e3b8b28a689a7175c53
Jack Lee <welsar55@gmail.com>
2023-02-22 12:13:37 -0600

    chage list to tuple

--- db.py
+++ db.py
@@ -57,16 +57,16 @@ def insert_review(loc_id: int, rating: str, tags: str, review: str, user_id: str
 def select_reviews(loc_id: int) -> list:
     with get_db_cursor() as cur:
         cur: cursor
-        cur.execute("SELECT rating, tags, review FROM Review WHERE loc_id = %s", ([loc_id]))
+        cur.execute("SELECT rating, tags, review FROM Review WHERE loc_id = %s", (loc_id,))
         return cur.fetchall()
     
 def get_location(loc_id):
     with get_db_cursor() as cur:
-        cur.execute("SELECT * FROM Location WHERE id = %s", ([loc_id]))
+        cur.execute("SELECT * FROM Location WHERE id = %s", (loc_id,))
         return cur.fetchone()
     
 def get_rating(loc_id: int) -> float:
     with get_db_cursor() as cur:
-        cur.execute("SELECT COALESCE(AVG(rating), 0.0) FROM Review WHERE loc_id = %s", ([loc_id]))
+        cur.execute("SELECT COALESCE(AVG(rating), 0.0) FROM Review WHERE loc_id = %s", (loc_id,))
         return float(cur.fetchone()[0])
     
