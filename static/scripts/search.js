f25e7f81cb48c885744b834738ee79a91e030e9d
Jack Lee <welsar55@gmail.com>
2023-03-05 17:12:59 -0600

    Have frontend make search request

--- static/scripts/search.js
+++ static/scripts/search.js
@@ -7,3 +7,27 @@ function hideFilters() {
     const filters = document.getElementById("filter-modal");
     filters.style.display = "none";
 }
+
+async function search(location, text, tags, minRating, proximity) {
+    fetch("/api/search?" + new URLSearchParams({
+        location: location.coords.latitude + "," + location.coords.longitude,
+        miles: proximity,
+        tags: tags.join(",")
+    })).then(response => response.json()).then(data => {
+        console.log(data);
+    });
+}
+
+let searchBar = document.getElementById("search");
+searchBar.addEventListener("keydown", event => {
+    if (event.key != "Enter")
+        return;
+
+    let tags = getPickedTags();
+    let minRating = document.getElementById("min-rating").value;
+    let text = searchBar.value;
+    let proximity = document.getElementById("proximity-input").value;
+    navigator.geolocation.getCurrentPosition(function (location) {
+        search(location, text, tags, minRating, proximity);
+    }, function (positionError) { /* "Error Handling" */ } );
+});
