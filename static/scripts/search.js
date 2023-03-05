361036dc7fa1c21b86cdbed0c1fc97206117ed7d
Jack Lee <welsar55@gmail.com>
2023-03-05 10:48:11 -0600

    Add filter modal

---
+++ static/scripts/search.js
@@ -0,0 +1,9 @@
+function showFilters() {
+    const filters = document.getElementById("filter-modal");
+    filters.style.display = "block";
+}
+
+function hideFilters() {
+    const filters = document.getElementById("filter-modal");
+    filters.style.display = "none";
+}
