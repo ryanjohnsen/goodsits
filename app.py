f5ae6478f15290ff12ff5490c1363913927fed19
Jack Lee <welsar55@gmail.com>
2023-03-05 18:46:17 -0600

    Add the search results as cards

--- app.py
+++ app.py
@@ -84,7 +84,11 @@ def landing() -> Response:
 # Returns back a image link to a entry in the database
 @app.route("/images/<int:image_id>", methods = ["GET"])
 def image(image_id: int) -> Response:
-    image = b64decode(db.get_image(image_id))
+    binary = db.get_image(image_id)
+    if (binary == None):
+        abort(404)
+
+    image = b64decode(binary)
 
     mime = magic.from_buffer(image, mime = True)
     stream = BytesIO(image)
@@ -97,15 +101,19 @@ def search_api() -> Response:
     location = request.args.get("location", "0,0")
     miles = request.args.get("miles")
     tags = request.args.get("tags")
+    text = request.args.get("text")
+    min_rating = request.args.get("minRating")
 
-    locations, results = db.search_locations(location, miles, tags), []
+    locations, results = db.search_locations(location, text, miles, min_rating, tags), []
     for loc in locations:
         results.append({
             "id": loc[0],
             "title": loc[1],
             "hours": loc[2],
             "location": loc[3],
-            "tags": loc[4]
+            "distance": loc[4],
+            "tags": loc[5],
+            "rating": loc[6]
         })
     
     return jsonify(results)
--- static/scripts/search.js
+++ static/scripts/search.js
@@ -8,13 +8,48 @@ function hideFilters() {
     filters.style.display = "none";
 }
 
+function clearCards() {
+    document.getElementById("cards-container").innerHTML = "";
+}
+
+function addCard(location) {
+    // This definetly has cross site scripting vulnerabilities right now
+    let card = `
+        <a href="#">
+        <div class="card pure-g">
+            <div class="info pure-u-13-24">
+            <div class="heading">
+                <div class="title">${location.title}</div>
+                <div class="subtitle">${location.hours}</div>
+            </div>
+            <div class="details">
+                <div class="proximity">📌 ${location.distance} km away</div>
+                <div class="rating">⭐ ${location.rating == null ? "5.0" : location.rating} / 5.0</div>
+            </div>
+            <div id="tags">(tags go here)</div>
+            </div>
+            <div class="img-container pure-u-11-24">
+            <img class="img pure-img" src="/images/${location.id}" alt="">
+            </div>
+        </div>
+        </a>
+    `;
+    let div = document.createElement('div');
+    div.innerHTML = card;
+    document.getElementById("cards-container").appendChild(div);
+}
+
 async function search(location, text, tags, minRating, proximity) {
     fetch("/api/search?" + new URLSearchParams({
         location: location.coords.latitude + "," + location.coords.longitude,
+        text: text,
         miles: proximity,
+        minRating: minRating,
         tags: tags.join(",")
     })).then(response => response.json()).then(data => {
         console.log(data);
+        clearCards();
+        data.forEach(ele => addCard(ele));
     });
 }
 
