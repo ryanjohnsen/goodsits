bdfaa68542c78e21ea9fc14f8bf30c1579e29f0b
Youssef G Zahar <youssef@infra04-wg012.eduroam.wireless.umn.edu>
2023-03-07 14:28:50 -0600

    maxed tag selection to 3:no_entry_sign:

--- static/scripts/reviewTags.js
+++ static/scripts/reviewTags.js
@@ -6,13 +6,15 @@ function createTagElement(tag) {
     ele.classList.add("unpicked");
     ele.textContent = tag;
 
+
     ele.addEventListener("click", event => {
         ele.parentNode.removeChild(ele);
-        if (ele.classList.contains("unpicked")) {
+        if (ele.classList.contains("unpicked") && getPickedTags().length<3) {
             ele.classList.remove("unpicked");
             ele.classList.add("picked");
             let picked = document.getElementById("review-picked-tags");
             picked.appendChild(ele);
+            // console.log(getPickedTags().length);
         } else {
             ele.classList.remove("picked");
             ele.classList.add("unpicked");
@@ -38,7 +40,7 @@ function setupTags() {
     let unpicked = document.createElement("div");
     unpicked.id = "review-unpicked-tags";
     tags.appendChild(unpicked);
-    
+
     let picked = document.createElement("div");
     picked.id = "review-picked-tags"
     tags.appendChild(picked);
