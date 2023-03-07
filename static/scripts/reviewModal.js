65ce166fc664fa6855e4fa996a7bd80947013d5d
minizou <gh.mini.zou@gmail.com>
2023-03-07 17:54:29 -0600

    Prevent clicking when review popup is up; fix star alignment

--- static/scripts/reviewModal.js
+++ static/scripts/reviewModal.js
@@ -1,10 +1,14 @@
 function addReview() {
-    const reviewModal=document.getElementById("hideReviewForm");
+    const reviewModal = document.getElementById("hideReviewForm");
+    const overlay = document.getElementById("fade-out-container");
     reviewModal.style.display="block";
+    overlay.style.display="block";
 }
 
 function closeModal() {
-    const reviewModal=document.getElementById("hideReviewForm");
+    const reviewModal = document.getElementById("hideReviewForm");
+    const overlay = document.getElementById("fade-out-container");
+    overlay.style.display="none";
     reviewModal.style.display="none";
 }
 
